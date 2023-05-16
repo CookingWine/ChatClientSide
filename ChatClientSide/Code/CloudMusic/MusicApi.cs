57586202f5b09addb68771855a0dac1010556632
huanglekai <3181983989@qq.com>
2023-05-16 12:12:57 +0800

    添加配置文件

---
+++ ChatClientSide/Code/CloudMusic/MusicApi.cs
@@ -0,0 +1,17 @@
+﻿using ChatClientSide.Code.Utility;
+
+namespace ChatClientSide.Code.CloudMusic
+{
+    public class MusicApi
+    {
+        public static MusicApi? Instance { get; private set; }
+
+        public string? RequestUrl { get; private set; }
+
+        public MusicApi( )
+        {
+            Instance = this;
+            RequestUrl = UtilityTools.GetAppSetting( "MusicUrl" );
+        }
+    }
+}
---
+++ ChatClientSide/Code/Utility/UtilityTools.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+
+namespace ChatClientSide.Code.Utility
+{
+    public class UtilityTools
+    {
+        public static string GetAppSetting( string key )
+        {
+            return ConfigurationManager.AppSettings[key] ?? string.Empty;
+        }
+
+        public static int GetAppSettingInt( string key )
+        {
+            return int.Parse( GetAppSetting( key ) );
+
+        }
+    }
+}
